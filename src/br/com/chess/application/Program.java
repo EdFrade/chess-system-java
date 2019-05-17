e8400b20eed3a85072507bb42e0e73349e627f5b
Eduardo Frade <eduardofrad@hotmail.com>
2019-05-17 14:53:57 -0300

    Moving Pieces

--- src/br/com/chess/application/Program.java
+++ src/br/com/chess/application/Program.java
@@ -1,14 +1,27 @@
 package br.com.chess.application;
 
+import java.util.Scanner;
+
 import br.com.chess.chess.ChessMatch;
+import br.com.chess.chess.ChessPiece;
+import br.com.chess.chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-	
+		Scanner sc = new Scanner(System.in);
 		ChessMatch cm = new ChessMatch(); 
+		while(true) {
 		UI.printBoard(cm.getPieces());
+		System.out.println();
+		System.out.println("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		System.out.println();
+		System.out.println("Target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		ChessPiece capturedPiece = cm.performChessMove(source, target);
+		}
 	}
 
 }
--- src/br/com/chess/application/UI.java
+++ src/br/com/chess/application/UI.java
@@ -1,6 +1,10 @@
 package br.com.chess.application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import br.com.chess.chess.ChessPiece;
+import br.com.chess.chess.ChessPosition;
 import br.com.chess.chess.Color;
 
 public class UI {
@@ -25,6 +29,21 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+		String s = sc.nextLine();
+		char column = s.charAt(0);
+		int row = Integer.parseInt(s.substring(1));
+		return new ChessPosition(column, row);
+		}
+		catch(RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. valid values are from a1 to h8");
+			
+		}
+	}
+	
+	
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -34,7 +53,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println(" a b c d e f g  h");
+		System.out.println("  a b c d e f g  h");
 	}
 
 	private static void printPiece(ChessPiece piece) {
